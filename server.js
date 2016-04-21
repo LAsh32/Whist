9aec7e79138edcf8ba86c6020c1fe25f1267c297
Komodo123 <tmarcus97@gmail.com>
2016-04-21 13:12:41 -0700

    Hello world express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log ("Hello world");
+var express = require('express'),
+    app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
+});
